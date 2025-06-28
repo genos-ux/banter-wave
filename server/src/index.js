f11af0a1f8d2522ef804f081608fceb2a1268b91
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-06-28 20:29:25 +0000

    feat: created authRoutes and controllers endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import routes from './routes/index.routes.js';
 
 const app = express();
 
 
+app.use("/api/v1", routes);
+
+
 app.listen(4000, () => {
-    console.log(`Server is running on port 4001`);
+    console.log(`Server is running on port 4000`);
 })
