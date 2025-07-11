7e81481018280528a9223987f3ed7f95d1938168
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-07-11 13:42:25 +0000

    feat: implemented socket.io at the server side

--- server/src/index.js
+++ server/src/index.js
@@ -5,8 +5,8 @@ import connectToDatabase from './database/mongodb.js';
 import { errorMiddleware } from './middlewares/error.middleware.js';
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from './lib/socket.js';
 
-const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -23,7 +23,7 @@ app.use("/api/v1", routes);
 
 app.use(errorMiddleware);
 
-app.listen(PORT,  async() => {
+server.listen(PORT,  async() => {
   console.log(`BanterWave API is running on http://localhost:${PORT}`);
   await connectToDatabase();
 });
