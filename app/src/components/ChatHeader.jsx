107a2954fcb6551ff7660309b3ef06c298fe7251
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-07-11 16:48:37 +0000

    updated chatHeader component

--- app/src/components/ChatHeader.jsx
+++ app/src/components/ChatHeader.jsx
@@ -20,7 +20,7 @@ const ChatHeader = () => {
 
                 {/* User info */}
                 <div>
-                    <h3 className="font-medium">{selectedUser.fullName}</h3>
+                    <h3 className="font-medium">{selectedUser.userName}</h3>
                     <p className="text-sm text-base-content/70">
                         {onlineUsers.includes(selectedUser._id) ? "Online" : "Offline"}
                     </p>
