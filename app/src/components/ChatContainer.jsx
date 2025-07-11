16bc5183b4e608e09a1005f531dc7212aa9653cb
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-07-11 15:42:26 +0000

    wip: socket.js update

--- app/src/components/ChatContainer.jsx
+++ app/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
@@ -7,13 +7,22 @@ import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-  const { messages, getMessages, isMessagesLoading, selectedUser } =
+  const { messages, getMessages, isMessagesLoading, selectedUser, subscribeToMessages, unsubscribeFromMessages } =
     useChatStore();
   const {authUser} = useAuthStore();
+  const messageEndRef = useRef(null);
 
   useEffect(() => {
     getMessages(selectedUser._id);
-  }, [selectedUser._id, getMessages]);
+
+    subscribeToMessages();
+
+    return () => unsubscribeFromMessages();
+  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
+
+  useEffect(() => {
+    if(messageEndRef.current && messages) messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+  },[messages])
 
   if (isMessagesLoading)
     return (
@@ -35,6 +44,7 @@ const ChatContainer = () => {
             className={`chat ${
               message.senderId === authUser._id ? "chat-end" : "chat-start"
             }`}
+            ref={messageEndRef}
           >
             <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
@@ -50,7 +60,7 @@ const ChatContainer = () => {
             </div>
 
             <div className="chat-header mb-1">
-              <time datetime="" className="text-xs opacity-50 ml-1">
+              <time dateTime="" className="text-xs opacity-50 ml-1">
                 {formatMessageTime(message.createdAt)}
               </time> 
             </div>
